ea5ea4ae1b058daea85dcfcb9ba370796cf99869
Colum Ferry <colum.ferry@columferry.co.uk>
2017-01-26 14:59:34 +0000

    Setup the Pages and Views

---
+++ System/Libraries/UWebAdmin/Pages/Login/Login_Page.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Colum
+ * Date: 26/01/2017
+ * Time: 14:51
+ */
+
+namespace System\Libraries\UWebAdmin\Pages\Login;
+
+
+use Untitled\PageBuilder\Page;
+
+class Login_Page extends Page
+{
+
+}
---
+++ System/Libraries/UWebAdmin/Pages/Register/Register_Page.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Colum
+ * Date: 26/01/2017
+ * Time: 14:51
+ */
+
+namespace System\Libraries\UWebAdmin\Pages\Register;
+
+
+use Untitled\PageBuilder\Page;
+
+class Register_Page extends Page
+{
+
+}
