b65efdce5988625837434edd8e673f4287199bcc
Colum Ferry <colum.ferry@columferry.co.uk>
2017-02-05 17:29:56 +0000

    Add a few API methods

--- System/Libraries/UWebAdmin/Models/API/Api.php
+++ System/Libraries/UWebAdmin/Models/API/Api.php
@@ -10,11 +10,16 @@ namespace System\Libraries\UWebAdmin\Models\API;
 
 
 use System\Libraries\UWebAdmin\Config\UWA_Config;
+use System\Libraries\UWebAdmin\Models\Dashboard\Activity;
 use System\Libraries\UWebAdmin\Models\Users\Role;
+use System\Libraries\UWebAdmin\Models\Users\User;
+use System\Libraries\UWebAdmin\UWA;
 use Untitled\Database\Database;
 
 class Api
 {
+
+    //region General API
 
     /**
      * @return array Roles objects
@@ -43,5 +48,84 @@ class Api
         }
         return $Menu;
     }
+
+    //endregion
+
+    //region Users API
+
+    /**
+     * Get all the registered users
+     * @return array User array
+     */
+    public static function GetUsers(){
+        $db = new Database(true);
+        $db->Run("SELECT id FROM ". UWA_Config::$USERS_TABLE, []);
+
+        $Users = [];
+        foreach($db->FetchAll(\PDO::FETCH_ASSOC) as $user) {
+            $Users[] = new User($user['id']);
+        }
+
+        return $Users;
+    }
+
+    //endregion
+
+    //region Latest Activity API
+
+    /**
+     * Get the latest activity that has occurred on the dashboard
+     * @param int $limit
+     * @return array
+     */
+    public static function GetLatestActivity($limit = null){
+
+        $sql = "SELECT * FROM ".
+            UWA_Config::$LATEST_ACTIVITY_TABLE .
+            !is_null($limit) ? " LIMIT" . $limit : "" .
+            " ORDER BY time DESC";
+
+        $db = new Database(true);
+
+        $db->Run($sql, []);
+
+        $Activity = [];
+        foreach($db->FetchAll(\PDO::FETCH_ASSOC) as $activity){
+            $a = new Activity();
+            $a->Id = $activity['id'];
+            $a->User = new User($activity['user_id']);
+            $a->Activity = $activity['activity'];
+            $a->Time = $activity['time'];
+
+            $Activity[] = $a;
+        }
+
+        return $Activity;
+    }
+
+    /**
+     * Add a new activity
+     * @param Activity $activity
+     */
+    public static function AddActivity($activity){
+        $activity->Insert();
+    }
+
+    /**
+     * Add a new activity
+     * @param User $user
+     * @param string $activity
+     * @param string $time
+     */
+    public static function NewActivity($user, $activity, $time){
+        $a = new Activity();
+        $a->User = $user;
+        $a->Activity = $activity;
+        $a->Time = $time;
+
+        $a->Insert();
+    }
+
+    //endregion
 
 }
