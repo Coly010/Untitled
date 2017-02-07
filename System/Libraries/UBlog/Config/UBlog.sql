e26364ccfb0b5fc051a36aef0a2b342cd7c5f60a
Colum Ferry <colum.ferry@columferry.co.uk>
2017-02-07 19:19:02 +0000

    Initial setup
    Add UBlog sql
    Add UBlog plugin

---
+++ System/Libraries/UBlog/Config/UBlog.sql
@@ -0,0 +1,46 @@
+DROP TABLE IF EXISTS ub_blogs;
+
+CREATE TABLE ub_blogs (
+
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) DEFAULT NULL,
+  description LONGTEXT DEFAULT NULL,
+
+  PRIMARY KEY (id)
+
+)ENGINE=InnoDb;
+
+
+DROP TABLE IF EXISTS ub_blog_posts;
+
+CREATE TABLE ub_blog_posts (
+
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  blog INT(11) NOT NULL,
+  user INT(11) NOT NULL,
+  title VARCHAR(100) NOT NULL,
+  content LONGTEXT NOT NULL,
+  likes MEDIUMTEXT DEFAULT NULL,
+  visible INT(1) DEFAULT 1,
+  time VARCHAR(15) DEFAULT NULL,
+
+  PRIMARY KEY (id)
+
+)ENGINE=InnoDb;
+
+
+DROP TABLE IF EXISTS ub_blog_comments;
+
+CREATE TABLE ub_blog_comments (
+
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  post INT(11) NOT NULL,
+  user INT(11) NOT NULL,
+  comment VARCHAR(255) DEFAULT NULL,
+  likes MEDIUMTEXT DEFAULT NULL,
+  visible INT(1) DEFAULT 1,
+  time VARCHAR(15) DEFAULT NULL,
+
+  PRIMARY KEY (id)
+
+)ENGINE=InnoDb;
---
+++ System/Libraries/UBlog/UBlog.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Colum
+ * Date: 07/02/2017
+ * Time: 19:05
+ */
+
+namespace System\Libraries\UBlog;
+
+
+use System\Libraries\Interfaces\IPlugin;
+
+class UBlog implements IPlugin
+{
+
+    public static function Start()
+    {
+
+    }
+
+}
