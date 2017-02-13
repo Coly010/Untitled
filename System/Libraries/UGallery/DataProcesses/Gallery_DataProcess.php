9929e95dee44dc0e1c9cd7f42fe73b6eada9ad4d
Colum Ferry <colum.ferry@columferry.co.uk>
2017-02-13 22:53:58 +0000

    Add dataprocess

---
+++ System/Libraries/UGallery/DataProcesses/Gallery_DataProcess.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Colum
+ * Date: 13/02/2017
+ * Time: 22:53
+ */
+
+namespace System\Libraries\UGallery\DataProcesses;
+
+
+use Untitled\PageBuilder\DataProcess;
+
+class Gallery_DataProcess extends DataProcess 
+{
+
+}
